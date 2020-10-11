70b210c4bfab3de082414ac21846dae8407a6d8e
idealistic91 <frerichs.jens@hotmail.de>
2020-10-11 19:15:17 +0200

    Fix env var

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,4 +115,4 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
 
-Rails.application.routes.default_url_options[:host] = ENV['host']
+Rails.application.routes.default_url_options[:host] = ENV['HOST']
