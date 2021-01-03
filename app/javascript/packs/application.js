f6f2e179ae8e45a8c251f72104d4b57431793c27
Jens Frerichs <frerichs@yfu.de>
2021-01-03 16:42:42 +0100

    Remove const images from application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -71,5 +71,5 @@ $(document).on('ajax:before', function(){
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-const images = require.context('../images', true)
+//const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
