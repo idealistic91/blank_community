ffa93f0e9902f67f6db82718603af721e380213d
idealistic91 <frerichs.jens@hotmail.de>
2021-02-14 13:02:07 +0100

    HOTFIX - Force ssl for production

--- lib/discord/bot.rb
+++ lib/discord/bot.rb
@@ -37,11 +37,11 @@ module Discord
         def build_registration_link(id, server_id = nil)
             params = { discord_id: id }
             params[:server_id] = server_id if server_id
-            new_user_registration_url(params, secure: true)
+            new_user_registration_url(params)
         end
 
         def build_community_config_link(id)
-            community_url(id, secure: true)
+            community_url(id)
         end
     end
 end
