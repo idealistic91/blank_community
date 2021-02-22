6f8aa94e4824415aefd5ed34121e55db1a95a7fb
idealistic91 <frerichs.jens@hotmail.de>
2021-02-22 19:14:15 +0100

    Change to perform_now

--- config/initializers/bot.rb
+++ config/initializers/bot.rb
@@ -5,7 +5,7 @@ require 'discord/bot'
 DISCORD_BOT ||= Discord::Bot.new
 DISCORD_BOT_SYNC = Discord::Bot.new
 InitializerHelpers.skip_console do
-    StartBotJob.perform if Rails.env.production? && !defined?(Rails::Server)
+    StartBotJob.perform_now if Rails.env.production? && !defined?(Rails::Server)
 end
 
 DISCORD_BOT.bot.run :async
