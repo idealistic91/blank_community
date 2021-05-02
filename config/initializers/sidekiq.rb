fb693c0fadefacc907846676af43a04331982200
jensFrerichs <frerichs@yfu.de>
2021-05-02 22:09:28 +0200

    32 - Use sidekiq as an adapter in dev environment

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,15 @@
+if Rails.env.development?
+  Sidekiq.configure_server do |config|
+    config.redis = {
+        host: ENV['REDIS_HOST'],
+        port: ENV['REDIS_PORT'] || '6379'
+    }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = {
+        host: ENV['REDIS_HOST'],
+        port: ENV['REDIS_PORT'] || '6379'
+    }
+  end
+end
