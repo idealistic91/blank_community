885568b9f0887f0db38f3aaacaac1d6e5d6a5caa
jensFrerichs <frerichs@yfu.de>
2021-05-02 22:08:23 +0200

    32 - Check for changes in after save callback

--- app/models/event_settings.rb
+++ app/models/event_settings.rb
@@ -1,10 +1,31 @@
 class EventSettings < ApplicationRecord
     belongs_to :event
 
+    after_save :check_changes
+
     EVENT_TYPES = {default: 'Standard', versus: 'Versus'}
-    NOTIFY_PARTICIPANTS = {dont: nil, five_min_before: 5.minutes, fiveteen_min_before: 15.minutes, half_an_hour_before: 30.minutes, hour_before: 1.hour}
+    NOTIFY_PARTICIPANTS = {dont: nil, five_min_before: 5.minutes, fiveteen_min_before: 15.minutes,
+                           half_an_hour_before: 30.minutes, hour_before: 1.hour}
 
     def dont_notify?
         notify_participants == 'dont'
+    end
+
+    def remind_server_has_changed?
+        self.remind_server != self.remind_server_before_last_save
+    end
+
+    def notify_participants_changed?
+        self.notify_participants != self.notify_participants_before_last_save
+    end
+
+    def check_changes
+        if remind_server_has_changed? || notify_participants_changed?
+            reinitialize_jobs
+        end
+    end
+
+    def reinitialize_jobs
+        event.reinitialize_jobs
     end
 end
