0eef425f603d6696ae89e1d56fe7ca4655e67c5a
idealistic91 <frerichs.jens@hotmail.de>
2021-12-10 20:56:20 +0100

    39 - Refactor actions

--- app/controllers/api/v1/communities.rb
+++ app/controllers/api/v1/communities.rb
@@ -2,8 +2,10 @@ module API
     module V1
       class Communities < Grape::API
         include API::V1::Defaults
+
         resource :communities do
             desc "Returns communities where I have a membership"
+            
             get "/my_communities" do
                 current_user.memberships.includes(:community).map(&:community)
             end
@@ -22,19 +24,23 @@ module API
             end
 
             desc 'Memberships of community'
+
             params do
                 requires :id, type: String, desc: "ID of the community"
             end
-            get '/:id/memberships' do
+
+            get '/:id/members' do
                 community = Community.where(id: permitted_params[:id]).first!
                 community.members
             end
 
             desc 'Events of communities'
+
             params do
                 requires :id, type: String, desc: "ID of the community"
                 requires :scope, type: String, values: ['upcoming_events', 'past_events', 'live_events'], desc: "Type of Events"
             end
+
             get '/:id/events' do
                 community = Community.where(id: permitted_params[:id]).first!
                 community.events.send(permitted_params[:scope]).includes(:games, members: [:user], hosts: [:user])
